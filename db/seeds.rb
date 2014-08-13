e90ce21ba416e1da7868c9c8bfe08873a413ff76
LCmry <lcrees.2014@gmail.com>
2014-08-12 19:25:26 -0700

    Created topics and added to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,22 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create(
+    name:        Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create(
     user:  users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+    body:  Faker::Lorem.paragraph
   )
 end
 posts = Post.all
@@ -30,10 +40,34 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes(
-  email: 'lc_monterey@hotmail.com',
-  password: 'helloworld'
+# Create an admin user
+admin = User.new(
+  name:      'Admin User',
+  email:     'admin@example.com',
+  password:  'helloworld',
+  role:       'admin'
 )
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name:          'Moderator User',
+  email:         'moderator@example.com',
+  password:      'helloworld',
+  role:          'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+name:        'Member User',
+email:       'member@example.com',
+password:    'helloworld'
+)
+member.skip_confirmation!
+member.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
