bf73b12482b43be47ea9647776ef1c44f55e1c28
LCmry <lcrees.2014@gmail.com>
2014-08-21 14:12:25 -0700

    Added voting model and specs

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,9 +4,9 @@ describe Vote do
   describe "validations" do
     context "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @vote.new(value: 1).valid? ).to eq(true)
-        expect( @vote.new(value: -1).valid? ).to eq(true)
-        expect( @vote.new(value: 2).valid? ).to eq(false)
+        expect( Vote.create(value: 1).valid? ).to eq(true)
+        expect( Vote.create(value: -1).valid? ).to eq(true)
+        expect( Vote.create(value: 2).valid? ).to eq(false)
       end
     end
   end
