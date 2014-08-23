9a481c70001e6a566f9e65b4359714e60cb13227
LCmry <lcrees.2014@gmail.com>
2014-08-23 13:44:20 -0700

    Added favorites model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ class User < ActiveRecord::Base
   has_many :posts
     mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :favorites, dependent: :destroy
 
   def role?(base_role)
     role == base_role.to_s
+  end
+
+  def favorited(post)
+    favorites.where(post_id: post.id).first
   end
 end
