d55a214413872212504875c024e2f1ad0afe41d1
LCmry <lcrees.2014@gmail.com>
2014-08-23 21:02:02 -0700

    Made topics private using scopes, added spec

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe Topic do
+  describe "scopes" do
+
+    before do
+      @public_topic = Topic.create
+      @private_topic = Topic.create(public: false)
+    end
+
+    context "public_topic" do
+      it "returns a relation of all public topics" do
+        expect(Topic.public_topic).to eq( [@public_topic] )
+      end
+    end
+
+    context "private_topic" do
+      it "returns a relation of all private topics" do
+        expect(Topic.private_topic).to eq( [@private_topic] )
+      end
+    end
+
+    context "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = true
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        user = nil
+        expect(Topic.visible_to(user)).to eq( [@public_topic] )
+      end
+    end
+  end
+end
