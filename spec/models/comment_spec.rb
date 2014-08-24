3a32c0e9a207a85bbd9f9e80335550124ed03867
LCmry <lcrees.2014@gmail.com>
2014-08-24 16:23:59 -0700

    Added email preference for favorites

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Comment do
 
-  include TestFactories
+  #include TestFactories
 
   context "after_create" do
 
@@ -12,22 +12,57 @@ describe Comment do
       @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
     end
 
-    it "sends an email to users who have favorited the post" do
-      @user.favorites.where(post: @post).create
+    context "with user's permission" do
 
-      allow( FavoriteMailer )
-        .to receive(:new_comment)
-        .with(@user, @post, @comment)
-        .and_return( double(deliver: true) )
+      it "sends an email to users who have favorited the post" do
+        @user.favorites.where(post: @post).create
 
-      @comment.save
+        allow( FavoriteMailer )
+          .to receive(:new_comment)
+          .with(@user, @post, @comment)
+          .and_return( double(deliver: true) )
+
+        @comment.save
+      end
+
+      it "does not send emails to users who haven't" do
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+
+        @comment.save
+      end
     end
 
-    it "does not send emails to users who haven't" do
-      expect( FavoriteMailer )
-        .not_to receive(:new_comment)
+    context "without permission" do
 
-      @comment.save
+      before { @user.update_attribute(:email_favorites, false) }
+
+      it "does not send emails, even to users who have favorited" do
+        @user.favorites.where(post: @post).create
+
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+
+        @comment.save
+      end
     end
   end
 end
+  def associated_post(options={})
+  post_options = {
+    title: 'Post title',
+    body: 'Post bodies must be pretty long.',
+    topic: Topic.create(name: 'Topic name'),
+    user: authenticated_user
+    }.merge(options)
+
+    Post.create(post_options)
+  end
+
+  def authenticated_user(options={})
+    user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+    user = User.new(user_options)
+    user.skip_confirmation!
+    user.save
+    user
+  end
