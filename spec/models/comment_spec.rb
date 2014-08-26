0222bf8656fb09c62dc0eb8a5098b9f19b1468a3
LCmry <lcrees.2014@gmail.com>
2014-08-26 12:33:12 -0700

    Cleaned up specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Comment do
 
-  #include TestFactories
+  include TestFactories
 
   context "after_create" do
 
@@ -48,21 +48,3 @@ describe Comment do
     end
   end
 end
-  def associated_post(options={})
-  post_options = {
-    title: 'Post title',
-    body: 'Post bodies must be pretty long.',
-    topic: Topic.create(name: 'Topic name'),
-    user: authenticated_user
-    }.merge(options)
-
-    Post.create(post_options)
-  end
-
-  def authenticated_user(options={})
-    user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-    user = User.new(user_options)
-    user.skip_confirmation!
-    user.save
-    user
-  end
