2549784ce0cb7769ef3ccd036ef945ec15f7a949
LCmry <lcrees.2014@gmail.com>
2014-08-27 20:36:04 -0700

    Added ajax to comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,24 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def create
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
+    @comments = @post.comments
+
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @new_comment = Comment.new
+
     authorize @comment
+
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment.  Please try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
